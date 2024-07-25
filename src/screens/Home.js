d9ba89239155f2bcd8debec00d47eec7f286129b
Rishi Raj <98014028+rajRishi22@users.noreply.github.com>
2024-07-25 10:51:25 +0530

    Changed backend url

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -10,7 +10,8 @@ function Home() {
   const [foodItem, setFoodItem] = useState([]);
 
   const loadData = async () => {
-    let response = await fetch('http://localhost:5000/api/foodData', {
+
+    let response = await fetch('https://zaykaexpress-backend.onrender.com/api/foodData', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' }
     });
