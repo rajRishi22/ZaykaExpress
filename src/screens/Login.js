074dc9d3d0c65bd2d166ac6aaaa5f87fe65ab971
Rishi Raj <98014028+rajRishi22@users.noreply.github.com>
2024-08-16 12:17:10 +0530

    Update Login.js

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -11,7 +11,7 @@ export default function Login() {
     const handleSubmit = async(e)=>{
         e.preventDefault();
         console.log(JSON.stringify({email:credentials.email,password:credentials.password}));
-        const response=await fetch("http://localhost:5000/api/loginuser",{
+        const response=await fetch("https://zayka-express-evl9.vercel.app/api/loginuser",{
             method:'POST',
             headers:{
                 'Content-Type':'application/json'
