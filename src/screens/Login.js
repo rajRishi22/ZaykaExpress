b758139751aaf68d479c446b2d0774fc5dff946c
rajRishi22 <rishiraj14ap2004raj@gmail.com>
2025-02-02 17:00:06 +0530

    testing

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -13,7 +13,7 @@ export default function Login() {
     const handleSubmit = async(e)=>{
         e.preventDefault();
         try {
-            const response=await fetch('http://localhost:5000/api/loginuser',{
+            const response=await fetch('https://zayka-express-evl9.vercel.app/api/loginuser',{
               method:'POST',
               headers:{
                 'Content-Type':'application/json'
