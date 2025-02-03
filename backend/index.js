60ea22387ee4b8d6e754321ec6bce82d18803845
rajRishi22 <rishiraj14ap2004raj@gmail.com>
2025-02-03 19:04:07 +0530

    test3

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.get('/',(req,res)=>{
 })
 app.use((req,res,next)=>{
     // https://zayka-express-f1j7-rajrishi22s-projects.vercel.app/
-    res.setHeader("Acess-Control-Allow-Origin","http://localhost:3000/");
+    res.setHeader("Acess-Control-Allow-Origin","https://zayka-express-f1j7-rajrishi22s-projects.vercel.app/");
     res.header("Acess-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
     );
